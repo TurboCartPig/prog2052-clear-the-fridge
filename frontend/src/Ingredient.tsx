29170103a90eea6968e60ca7e2e5f489a85285ae
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-10-06 23:29:44 +0200

    Ingredient can now be "opened"

--- frontend/src/Ingredient.tsx
+++ frontend/src/Ingredient.tsx
@@ -3,23 +3,34 @@ import "./tailwind.css";
 
 type IngredientProps = {
   name: string,
-  unit: string;
+  unit: string,
 };
 
 type IngredientState = {
-  amount: number;
+  amount: number,
+  isOpen: boolean,
 };
 
 
 class Ingredient extends React.Component<IngredientProps,IngredientState> {
-  state: IngredientState = {
-    amount: 1,
-  };
+  // state: IngredientState = {
+  //   amount: 1,
+  //   isOpen: false,
+  // }
+
+  constructor(props: IngredientProps) {
+    super(props)
+    this.state = {
+      amount: 1,
+      isOpen: false,
+    }
+  }
   
   render () {
     return (
       <div className="grid justify-items-center">
-        <button className="bg-red-200 w-2/5 h-12 rounded-lg text-xs">
+        <button className="bg-red-200 w-2/5 h-12 rounded-lg text-xs"
+      onClick={() => {this.setState({isOpen: !this.state.isOpen})}}>
         <div className="float-left ml-8">
         {this.props.name}
         </div>
@@ -27,12 +38,15 @@ class Ingredient extends React.Component<IngredientProps,IngredientState> {
         {this.amount()}
         </div>
         </button>
+        {this.state.isOpen && <div className="bg-red-100 w-2/5 h-12 rounded-lg text-xs"></div>}
       </div>);
   }
 
   amount() {
-    return (this.state.amount + this.props.unit)
+    return (this.state.amount + " " + this.props.unit)
   }
+
+
 }
 
 export default Ingredient
