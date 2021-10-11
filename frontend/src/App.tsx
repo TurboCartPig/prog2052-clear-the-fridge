c06731c2a5e2f71b57f96427c0e2c9ff56b7812c
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-10-12 00:09:25 +0200

    Add initial search component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Button from "./Button";
-import Ingredient, {Unit} from "./Ingredient";
+import Search from "./Search";
+import Ingredient, { Unit } from "./Ingredient";
 import "./tailwind.css";
 
 function App() {
 	return (
 		<div>
-		<Button/>
-        <Ingredient name='Carrot' unit={Unit.pcs}/>
-        <Ingredient name='Milk' unit={Unit.ml}/>
-        <Ingredient name='Beef' unit={Unit.g}/>
+			<Search />
+			<Ingredient name="Carrot" unit={Unit.pcs} />
+			<Ingredient name="Milk" unit={Unit.ml} />
+			<Ingredient name="Beef" unit={Unit.g} />
 		</div>
 	);
 }
