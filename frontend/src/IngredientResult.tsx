991676e75612c2b264f1b212edcd33649efa475b
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-10-14 00:10:25 +0200

    Make the ingredient result addable

--- frontend/src/IngredientResult.tsx
+++ frontend/src/IngredientResult.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Edit from "./res/edit.svg";
 import "./tailwind.css";
 
 /**
@@ -14,7 +15,10 @@ type IngredientResultProps = {
 /**
  * State of the ingredient result component.
  */
-type IngredientResultState = {};
+type IngredientResultState = {
+	// Has this ingredient been added?
+	added: boolean;
+};
 
 class IngredientResult extends React.Component<
 	IngredientResultProps,
@@ -22,14 +26,33 @@ class IngredientResult extends React.Component<
 > {
 	constructor(props: IngredientResultProps) {
 		super(props);
+
+		this.state = {
+			added: false,
+		};
+
+		this.onAdd = this.onAdd.bind(this);
 	}
 
 	render() {
 		return (
-			<div className="px-3 py-3 mx-auto my-1 w-98/100 rounded-lg bg-white">
-				{this.props.name}
+			<div className="grid justify-items-center py-3 mx-auto my-1 w-98/100 rounded-lg bg-white">
+				<div className="col-span-1">{this.props.name}</div>
+				<img
+					className="col-span-1 col-start-10"
+					src={this.state.added ? undefined : Edit}
+					onClick={this.onAdd}
+				></img>
 			</div>
 		);
+	}
+
+	/**
+	 * Called when the user clicks `add`.
+	 */
+	onAdd() {
+		this.setState({ added: true });
+		this.props.onAdd(this.props.name);
 	}
 }
 
