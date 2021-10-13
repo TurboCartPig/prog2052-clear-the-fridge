03306bd4caec01c98978a9892758e896bc22ad96
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-10-13 18:46:07 +0200

    Fix building for production and choosing plugins appropriately

--- frontend/webpack.config.mjs
+++ frontend/webpack.config.mjs
@@ -5,75 +5,79 @@ import path from "path";
 import url from "url";
 import zlib from "zlib";
 
-const isProduction = process.env.NODE_ENV === "production";
+export default (env, argv) => {
+	const isProduction = argv.mode === "production";
 
-export default {
-	entry: "./src/main.tsx",
-	output: {
-		filename: "[name].bundle.js",
-		path: path.resolve(
-			path.dirname(url.fileURLToPath(import.meta.url)),
-			"./dist"
-		),
-	},
-	resolve: {
-		extensions: [".tsx", ".ts", ".js"],
-	},
-	devtool: isProduction ? undefined : "inline-source-map",
-	devServer: {
-		static: "./dist",
-	},
-	optimization: {
-		minimize: true,
-		runtimeChunk: "single",
-	},
-	plugins: [
+	// Webpack plugins
+	let plugins = [
 		new HtmlWebpackPlugin({
 			template: "./src/index.html",
 		}),
 		new MiniCssExtractPlugin(),
-		// Only do compression when in building for production
-		() => {
-			isProduction &&
-				new CompressionPlugin({
-					filename: "[path][base].gz",
-					test: /\.(js|css|html)$/i,
-					algorithm: "gzip",
-				});
-		},
-		() => {
-			isProduction &&
-				new CompressionPlugin({
-					filename: "[path][base].br",
-					test: /\.(js|css|html)$/i,
-					algorithm: "brotliCompress",
-					compressionOptions: {
-						params: {
-							[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-						},
+	];
+
+	// Add compression plugins in production
+	if (isProduction) {
+		plugins = plugins.concat([
+			new CompressionPlugin({
+				filename: "[path][base].gz",
+				test: /\.(js|css|html)$/i,
+				algorithm: "gzip",
+			}),
+			new CompressionPlugin({
+				filename: "[path][base].br",
+				test: /\.(js|css|html)$/i,
+				algorithm: "brotliCompress",
+				compressionOptions: {
+					params: {
+						[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
 					},
-				});
+				},
+			}),
+		]);
+	}
+
+	return {
+		entry: "./src/main.tsx",
+		output: {
+			filename: "[name].bundle.js",
+			path: path.resolve(
+				path.dirname(url.fileURLToPath(import.meta.url)),
+				"./dist"
+			),
 		},
-	],
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/i,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader",
-					"postcss-loader",
-				],
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
+		resolve: {
+			extensions: [".tsx", ".ts", ".js"],
+		},
+		devtool: isProduction ? undefined : "inline-source-map",
+		devServer: {
+			static: "./dist",
+		},
+		optimization: {
+			minimize: true,
+			runtimeChunk: "single",
+		},
+		plugins: plugins,
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/i,
+					use: [
+						MiniCssExtractPlugin.loader,
+						"css-loader",
+						"postcss-loader",
+					],
+				},
+				{
+					test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+					type: "asset/resource",
+				},
+			],
+		},
+	};
 };
