1f725acd9f98f7e520500ada2e8d237e5ea88d6e
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-11-07 20:21:29 +0100

    Move router constructor into api package

--- backend/api/common.go
+++ backend/api/common.go
@@ -1,6 +1,12 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
 
 // Middleware for setting content-type to json.
 func ReturnJSON(next http.Handler) http.Handler {
@@ -15,4 +21,31 @@ func ReturnMessage(msg string, code int) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, msg, code)
 	})
+}
+
+// Construct a new router for the API.
+func NewRouter() *chi.Mux {
+	// Setup new chi router
+	r := chi.NewRouter()
+
+	// Setup middleware
+	r.Use(middleware.Logger)
+	r.Use(ReturnJSON)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://clearthefridge.com", "https://clearthefridge.com","http://localhost:4080", "https://localhost:4080"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Content-Type"},
+		AllowCredentials: false,
+	}))
+
+	// Setup routes
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Get("/diag", NewDiagHandler())
+		r.Get("/ingredients", NewIngredientsHandler())
+		r.Get("/ingredients/search", NewIngredientsSearchHandler())
+		r.Get("/recipes", NewRecipesHandler())
+		r.Get("/recipes/search", NewRecipesSearchHandler())
+	})
+
+	return r
 }
--- backend/main.go
+++ backend/main.go
@@ -5,46 +5,16 @@ import (
 	"clearthefridge/db"
 	"log"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
-
-// Serve the REST API over HTTP
-func serve() {
-	// Setup new chi router
-	r := chi.NewRouter()
-
-	// Setup middleware
-	r.Use(middleware.Logger)
-	r.Use(api.ReturnJSON)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://clearthefridge.com", "https://clearthefridge.com","http://localhost:4080", "https://localhost:4080"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Content-Type"},
-		AllowCredentials: false,
-	}))
-
-	// Setup routes
-	r.Route("/api/v1", func(r chi.Router) {
-		r.Get("/diag", api.NewDiagHandler())
-		r.Get("/ingredients", api.NewIngredientsHandler())
-		r.Get("/ingredients/search", api.NewIngredientsSearchHandler())
-		r.Get("/recipes", api.NewRecipesHandler())
-		r.Get("/recipes/search", api.NewRecipesSearchHandler())
-	})
-
-	log.Fatal(http.ListenAndServe(":3080", r))
-}
 
 func main() {
 	log.Print("Starting")
 
 	db.ConnectDatabase()
 
-	// FIXME: serve() never returns, database does not disconnect
-	serve()
+	// FIXME: ListenAndServe() never returns, database does not disconnect
+	r := api.NewRouter()
+	log.Fatal(http.ListenAndServe(":3080", r))
 
 	db.DisconnectDatabase()
 }
